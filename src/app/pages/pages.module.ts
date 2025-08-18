f7923e82c4784c514b96f7ec4732707f7df4f7f7
Laís Batista <lais.batista.pereira@gmail.com>
2025-08-18 18:12:12 -0300

    ✨ feat: implement usuarios component and update routing to redirect to usuarios page.

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,17 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { PagesRoutingModule } from './pages-routing.module';
-import { HomeComponent } from './home/home.component';
-
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    PagesRoutingModule
-  ]
+  declarations: [UsuariosComponent],
+  imports: [CommonModule, PagesRoutingModule],
 })
-export class PagesModule { }
+export class PagesModule {}
