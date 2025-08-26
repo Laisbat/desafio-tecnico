11eb1b5a92a5e192892e5864d472c7a2e5f864d4
Laís Batista <lais.batista.pereira@gmail.com>
2025-08-26 13:59:18 -0300

    ✨ feat: implement Usuarios component with user listing, filtering, and actions; enhance styles and add necessary modules.

--- src/app/pages/usuarios/usuarios.component.spec.ts
+++ src/app/pages/usuarios/usuarios.component.spec.ts
@@ -8,9 +8,8 @@ describe('UsuariosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UsuariosComponent]
-    })
-    .compileComponents();
+      declarations: [UsuariosComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UsuariosComponent);
     component = fixture.componentInstance;
